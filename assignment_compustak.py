38e796e9b0b75e4508afc5891543365ce2adc4ca
mahgieeee <10890349+mahgieeee@users.noreply.github.co>
2018-03-29 00:56:13 -0400

    commit

--- assignment_compustak.py
+++ assignment_compustak.py
@@ -20,13 +20,13 @@ def pairs(input):
     # put the tuples in a list and return the list
     unique_tuples = []
     for key, values in unique_pairs.items():
-        print (key)
+        #print (key)
         for numbers in values:
-            print ("num: ", numbers)
+            #print ("num: ", numbers)
             #return (key, numbers)
-            if (key, numbers) not in unique_tuples: # no repeat
+            if (key, numbers) not in unique_tuples and key != numbers: # no repeat
                 unique_tuples.append((key, numbers))
-            
+                
     return unique_tuples
     # Solution goes here
 
@@ -46,7 +46,7 @@ def is_four_of_kind(hand):
                 count = count + 1 # number of unique strings in the hand
             next_rank = next_rank + 1
     # for 4/5 strings: only can be 2 unique letters 
-    print(count)
+    #print(count)
     sum_rank1 = 0
     sum_rank2 = 0
     if count == 2:
@@ -55,8 +55,8 @@ def is_four_of_kind(hand):
                 sum_rank1 = sum_rank1 + 1
             elif rank == diff_hand[1]:
                 sum_rank2 = sum_rank2 + 1
-        print(sum_rank1)
-        print(sum_rank2)
+        #print(sum_rank1)
+        #print(sum_rank2)
         if sum_rank1 == 4 or sum_rank2 == 4:
             return True
         return False
@@ -101,17 +101,20 @@ def dec_to_base_x(base, num):
         while num - base**start_exp >= 0:
             num = num - base**start_exp
             count = count + 1
-            print(num)
+            #print(num)
         num_count.append(count)
-            
+        
         start_exp = start_exp - 1
 
-    print (num_count)   
+    converted_num = " "
+    for numbers in num_count:
+        converted_num = converted_num + str(numbers)
+    return int(converted_num)    
     # Solution goes here
     
 if __name__ == '__main__':
-    #print(pairs([8, 8, 8, 8, 8]))
+    print(pairs([2, 8, 8, 8, 8]))
     print(is_four_of_kind(["5","5","K","5","5"]))
     #merge([1, 3, 5, 10], [2, 4, 4, 6, 8])
-    #dec_to_base_x(8, 140)
-    #dec_to_base_x(2, 8) 
+    print(dec_to_base_x(8, 140))
+    print(dec_to_base_x(2, 8)) 
